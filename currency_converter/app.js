e5050f3985d348ae2bed03bfaaa11720c79dbae3
Sneha <95482962+snehaj6@users.noreply.github.com>
2024-09-18 21:53:54 +0530

    Add files via upload

---
+++ currency_converter/app.js
@@ -0,0 +1,59 @@
+const BASE_URL =
+  "https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies";
+
+const selects = document.querySelectorAll(".content select");
+const btn = document.querySelector("form  button");
+const fromCurr = document.querySelector(".from select");
+const toCurr = document.querySelector(".to select");
+const exchange = document.querySelector("#exchange");
+const amount = document.querySelector("#amount");
+
+for (let select of selects) {
+  for (currCode in countryList) {
+    let newOption = document.createElement("option");
+    newOption.innerText = currCode;
+    newOption.value = currCode;
+    if (select.name === "from" && currCode === "USD") {
+      newOption.selected = "selected";
+    } else if (select.name === "to" && currCode === "INR") {
+      newOption.selected = "selected";
+    }
+    select.append(newOption);
+  }
+
+  select.addEventListener("change", (evt) => {
+    updateFlag(evt.target);
+  });
+}
+
+const updateExchangeRate = async () => {
+  let amtVal = amount.value;
+  if (amtVal === "" || amtVal < 1) {
+    amtVal = 1;
+    amount.value = "1";
+  }
+  const URL = `${BASE_URL}/${fromCurr.value.toLowerCase()}.json`;
+  let response = await fetch(URL);
+  let data = await response.json();
+  let rate = data[fromCurr.value.toLowerCase()][toCurr.value.toLowerCase()];
+  let finalAmount = amtVal * rate;
+  exchange.value = `${finalAmount} `;
+};
+
+const updateFlag = async (element) => {
+  let currCode = element.value;
+  let countryCode = countryList[currCode];
+  let newSrc = `https://flagsapi.com/${countryCode}/flat/64.png`;
+  let img = element.parentElement.querySelector("img");
+  img.src = newSrc;
+  updateExchangeRate();
+};
+
+btn.addEventListener("click", (evt) => {
+  evt.preventDefault();
+  updateExchangeRate();
+});
+
+window.addEventListener("load", () => {
+  updateExchangeRate();
+});
